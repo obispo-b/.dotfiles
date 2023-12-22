36c610326f3abe9ab7670065745f78d03c1b164b
Bruno <obispobruno@tuta.com>
2023-12-22 11:10:02 -0300

    fix: oh my zsh in end of script

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -76,10 +76,6 @@ git clone --bare https://github.com/obispobruno/.dotfiles.git ~/.dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
 
 git clone https://github.com/DreymaR/BigBagKbdTrixXKB.git ~/Downloads/BigBagKbdTrixXKB
 sudo ./Downloads/BigBagKbdTrixXKB/install-dreymar-xmod.sh
@@ -89,31 +85,33 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsM
 unzip ~/.local/share/fonts/JetBrainsMono.zip -d ~/.local/share/fonts
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  
+
 read -rp "Install flatpaks? [Y/n] " flat
 
-if [[ $flat == 'n' ]]; then
-  exit 0
+if [[ $flat != 'n' ]]; then
+	flatpak install -y com.github.tchx84.Flatseal \
+		com.discordapp.Discord \
+		org.cryptomator.Cryptomator \
+		io.gitlab.librewolf-community \
+		io.freetubeapp.FreeTube \
+		com.tutanota.Tutanota \
+		com.github.iwalton3.jellyfin-media-player \
+		org.libreoffice.LibreOffice \
+		org.keepassxc.KeePassXC \
+		org.localsend.localsend_app \
+		org.filezillaproject.Filezilla \
+		com.github.IsmaelMartinez.teams_for_linux \
+		com.github.xournalpp.xournalpp \
+		com.valvesoftware.Steam \
+		io.github.mimbrero.WhatsAppDesktop \
+		io.mpv.Mpv \
+		net.cozic.joplin_desktop \
+		net.davidotek.pupgui2 \
+		com.valvesoftware.Steam.CompatibilityTool.Proton-GE \
+		org.pulseaudio.pavucontrol
 fi
 
-flatpak install -y com.github.tchx84.Flatseal \
-com.discordapp.Discord \
-org.cryptomator.Cryptomator \
-io.gitlab.librewolf-community \
-io.freetubeapp.FreeTube \
-com.tutanota.Tutanota \
-com.github.iwalton3.jellyfin-media-player \
-org.libreoffice.LibreOffice \
-org.keepassxc.KeePassXC \
-org.localsend.localsend_app \
-org.filezillaproject.Filezilla \
-com.github.IsmaelMartinez.teams_for_linux \
-com.github.xournalpp.xournalpp \
-com.valvesoftware.Steam \
-io.github.mimbrero.WhatsAppDesktop \
-io.mpv.Mpv \
-net.cozic.joplin_desktop \
-net.davidotek.pupgui2 \
-com.valvesoftware.Steam.CompatibilityTool.Proton-GE \
-org.pulseaudio.pavucontrol
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
