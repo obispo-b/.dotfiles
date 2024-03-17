f74d9337528891cd54f63398c1fd7c4759f779a8
bruno <obispobruno@tuta.com>
2024-03-17 20:50:47 -0300

    astah fedora installer

--- .local/bin/fedora_astah_installer.sh
+++ .local/bin/fedora_astah_installer.sh
@@ -9,7 +9,7 @@ gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public"
 
 echo "$adoptiumrepo" > /etc/yum.repos.d/adoptium.repo
 
-sudo dnf install temurin-8-jdk
+sudo dnf install -y temurin-8-jdk
 
 wget -P /tmp https://cdn.change-vision.com/files/astah-uml-9.2.0.0248cd-0.noarch.rpm
 
