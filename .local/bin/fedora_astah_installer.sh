3450ff2c44e3f97536180b29182a2341b81ff90c
bruno <obispobruno@tuta.com>
2024-03-17 19:15:48 -0300

    astah installer fedora

---
+++ .local/bin/fedora_astah_installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+adoptiumrepo="[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/39/x86_64/
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public"
+
+echo "$adoptiumrepo" | sudo tee -a /etc/yum.repos.d/adoptium.repo >/dev/null
+
+sudo dnf install temurin-8-jdk
+
+wget -P /tmp https://cdn.change-vision.com/files/astah-uml-9.2.0.0248cd-0.noarch.rpm
+
+rpm -ivh --nosignature /tmp/astah-uml-9.2.0.0248cd-0.noarch.rpm
