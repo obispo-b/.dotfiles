6f6aa2578222de0c4f1ef26d0b0275b72ba7b442
Bruno Obispo <obispobruno@tuta.com>
2024-03-23 13:11:59 -0300

    facu

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -18,6 +18,8 @@ keepcache=True"
 
 echo "$faster_dnf" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
 
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 sudo dnf upgrade -y
 
 yes | sudo dnf copr enable pennbauman/ports
@@ -47,6 +49,7 @@ sudo dnf install -y \
 	bat \
 	xclip \
 	xsel \
+  eza \
 	neofetch \
 	p7zip \
 	p7zip-plugins \
