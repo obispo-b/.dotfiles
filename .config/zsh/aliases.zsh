398aa689b01b0cf81b5ba3a4220f538e8fd09574
Bruno Obispo <obispobruno@tuta.com>
2024-04-01 22:10:31 -0300

    yz alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -31,6 +31,7 @@ alias ls='exa'
 alias la='exa -la'
 alias ll='exa -l'
 alias l='exa -l'
+alias yz='yazi'
 alias fwine="flatpak run org.winehq.Wine"
 alias gpudriver="glxinfo | grep -iE 'vendor:|device:|version:'"
 alias updategrub="sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
